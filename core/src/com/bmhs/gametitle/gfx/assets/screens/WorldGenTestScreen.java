95567206457c86d23f69c40a44f75a8442a72cac
ezhou <ericzhou51@gmail.com>
2024-02-05 14:08:11 -0800

    commit

--- core/src/com/bmhs/gametitle/gfx/assets/screens/WorldGenTestScreen.java
+++ core/src/com/bmhs/gametitle/gfx/assets/screens/WorldGenTestScreen.java
@@ -15,9 +15,11 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 
+import com.bmhs.gametitle.game.assets.characters.PlayerCharacter;
 import com.bmhs.gametitle.game.utils.GameHandler;
 import com.bmhs.gametitle.game.assets.worlds.World;
 import com.bmhs.gametitle.gfx.assets.tiles.Tile;
+import com.bmhs.gametitle.gfx.utils.TileHandler;
 
 public class WorldGenTestScreen implements Screen {
 
@@ -31,6 +33,8 @@ public class WorldGenTestScreen implements Screen {
     private float effectiveViewportWidth, effectiveViewportHeight;
 
     private World world;
+
+    private PlayerCharacter player;
 
 
     public WorldGenTestScreen (final GameHandler game, final Screen parent) {
@@ -50,6 +54,8 @@ public class WorldGenTestScreen implements Screen {
         world = new World(100, 167);
 
         camera.update();
+
+        player = new PlayerCharacter(TileHandler.getTileHandler().getWorldTileArray().get(2), 100, 200);
     }
 
     @Override
@@ -78,12 +84,13 @@ public class WorldGenTestScreen implements Screen {
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
 
-            for (int r = 0; r < world.getWorldMapRows(); r++) {
-                for (int c = 0; c < world.getWorldMapColumns(); c++) {
-                    game.batch.draw(world.getWorldTileTextureRegion(r, c), c * Tile.ON_SCREEN_DEFAULT_WIDTH, r * Tile.ON_SCREEN_DEFAULT_HEIGHT);
-                }
+        for (int r = 0; r < world.getWorldMapRows(); r++) {
+            for (int c = 0; c < world.getWorldMapColumns(); c++) {
+                game.batch.draw(world.getWorldTileTextureRegion(r, c), c * Tile.ON_SCREEN_DEFAULT_WIDTH, r * Tile.ON_SCREEN_DEFAULT_HEIGHT);
             }
+        }
 
+        game.batch.draw(player.getTile().getTexture(), player.getX(), player.getY());
 
         game.batch.end();
 
@@ -135,6 +142,10 @@ public class WorldGenTestScreen implements Screen {
         }
         if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
             camera.zoom -= 0.2;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            player.adjustY(10);
         }
 
         if(Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)){
