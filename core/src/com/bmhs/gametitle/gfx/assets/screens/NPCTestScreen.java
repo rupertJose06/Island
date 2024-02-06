908326dced321da9cefa818babcc836485077a10
ezhou <ericzhou51@gmail.com>
2024-02-06 08:56:15 -0800

    commit

---
+++ core/src/com/bmhs/gametitle/gfx/assets/screens/NPCTestScreen.java
@@ -0,0 +1,72 @@
+package com.bmhs.gametitle.gfx.assets.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g3d.particles.ParticleSorter;
+import com.badlogic.gdx.utils.Array;
+import com.bmhs.gametitle.game.assets.characters.NonPlayerCharacter;
+import com.bmhs.gametitle.game.assets.worlds.World;
+import com.bmhs.gametitle.game.utils.GameHandler;
+import com.bmhs.gametitle.gfx.assets.tiles.statictiles.WorldTile;
+import com.bmhs.gametitle.gfx.utils.TileHandler;
+
+public class NPCTestScreen implements Screen {
+
+    GameHandler game;
+    Screen parent;
+
+    Array<NonPlayerCharacter> npcArray;
+
+    public NPCTestScreen(GameHandler game, Screen parent) {
+        this.game = game;
+        this.parent = parent;
+
+        npcArray = new Array<>();
+
+        int numOfNPCs = 30;
+        for(int i = 0; i < numOfNPCs; i++) {
+            WorldTile tempTile = TileHandler.getTileHandler().getWorldTileArray().get(2);
+            float tempX = (float)Math.random() * Gdx.graphics.getWidth();
+            float tempY = (float)Math.random() * Gdx.graphics.getHeight();
+            String tempName = "NPC " + i;
+
+            npcArray.add(new NonPlayerCharacter(tempTile, tempX, tempY, tempName));
+        }
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
